1f3a5e676146106869cabe9fadadce62b3c9ed3f
Elleni Toumpas <s3708633@student.rmit.edu.au>
2018-08-26 10:31:09 +1000

    More Importing and Cleaning

---
+++ 01_import_clean_convert_to_function.R
@@ -0,0 +1,180 @@
+######################################################################
+## PACKAGES
+######################################################################
+library(readr)
+library(data.table)
+library(tidyr)
+library(lubridate)
+library(stringr)
+
+## set your own working directory 
+setwd("/Users/ellenitoumpas/Dropbox/DATA/DATATHON/datathon/melbourne-datathon/")
+
+
+
+######################################################################
+## IMPORT FILES (except touch on and touch off)
+######################################################################
+calendar <- read.table("calendar.txt", sep="|", header=FALSE, col.names = c("DateSequence", "Date" ,"CalendarYear" ,"FinancialYear" ,"FinancialMonth" ,"CalendarMonth" ,"CalendarMonthSeq" ,"CalendarQuarter" ,"FinancialQuarter" ,"CalendarWeek" ,"FinancialWeek" ,"DayType" ,"DayTypeCategory", "DayTypeCategoryMore","WeekdaySeq" ,"WeekDay" ,"FinancialMonthSeq" ,"FinancialMonthName" ,"MonthNumber" ,"ABSWeek" ,"WeekEnding" ,"QuarterName"))
+car_speeds <- read_csv("car_speeds/melbourne_vehicle_traffic.csv")
+card_type <- read.table("card_types.txt", sep="|", header=FALSE, col.names = c("Card_SubType_ID" , "Card_SubType_Desc", "Payment_Type", "Fare_Type", "Concession_Type", "MI_Card_Group"))
+stoplocations <- read.table("stop_locations.txt", sep="|", fill = TRUE, col.names=c("StopLocationID" , "StopNameShort", "StopNameLong", "StopType", "SuburbName", "PostCode", "RegionName", "LocalGovernmentArea", "StatDivision", "GPSLat", "GPSLong"))
+
+
+
+######################################################################
+## IMPORT TXT FILES IN SUBFOLDERS - COPIED FROM ORGANISERS NOTES
+######################################################################
+
+whichSamp_on <- function(samp){
+  #tell R where it can find the data
+  ScanOnFolderMaster <- 'Samp_X/ScanOnTransaction'
+  ScanOnFolder <- sub("X",samp,ScanOnFolderMaster)
+  FolderType <- 'On'
+  
+  #list the files
+  onFiles <- list.files(ScanOnFolder,recursive = TRUE,full.names = TRUE)
+  
+  CreateDataFrame(offFiles, samp, FolderType)
+}
+
+whichSamp_off <- function(samp){
+  
+  #tell R where it can find the data
+  ScanOffFolderMaster <- 'Samp_X/ScanOffTransaction'
+  ScanOffFolder <- sub("X",samp,ScanOffFolderMaster)
+  FolderType <- 'Off'
+  
+  #list the files
+  offFiles <- list.files(ScanOffFolder,recursive = TRUE,full.names = TRUE)
+  
+  CreateDataFrame(offFiles, samp, FolderType)
+}
+
+
+CreateDataFrame <- function(offFiles, samp, FolderType){
+  # set up an empty dataframe for all of touch off data
+  EmptyDataFrame <- data.frame(Mode=integer(),
+                                BusinessDate=character(), 
+                                DateTime=character(), 
+                                CardID = integer(),
+                                CardType = integer(),
+                                VehicleID = integer(),
+                                ParentRoute = character(),
+                                RouteID = integer(),
+                                StopID = integer(),
+                                stringsAsFactors=FALSE) 
+  
+  DataFrameName <- paste('scan',FolderType,'_all_samp',samp)
+  }
+
+
+
+
+
+
+whichSamp_on(samp)
+whichSamp_off(samp)
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------
+#read in all touch off files and collate to one file
+#---------------------------------------------------
+
+for (i in 1:157){
+  
+  # which file are we importing
+  myFile <- offFiles[i]
+  
+  # import file
+  cmd <- paste0("gzip -dc ", myFile)
+  dt <- fread(cmd,nrow=10000,sep="|",fill=TRUE)
+  
+  # bind imported file to the main dataframe
+  scanOff_all_samp0 <- rbind(as.data.frame(scanOff_all_samp0), as.data.frame(dt))
+  
+}
+
+colnames(scanOff_all_samp0) <- c('Mode','BusinessDate','DateTime','CardID','CardType','VehicleID','ParentRoute','RouteID','StopID')
+head(scanOff_all_samp0)
+
+#---------------------------------------------------
+#read in all touch on files and collate to one file
+#---------------------------------------------------
+
+for (i in 1:157){
+  # which file are we importing
+  myFile <- onFiles[i]
+  
+  # import file
+  cmd <- paste0("gzip -dc ", myFile)
+  dt <- fread(cmd,nrow=10000,sep="|",fill=TRUE)
+  
+  # bind imported file to the main dataframe
+  scanOn_all_samp0 <- rbind(as.data.frame(scanOn_all_samp0), as.data.frame(dt))
+  
+}
+
+write.csv(scanOn_all_samp0, file = "all_scanOff_scanOn/all_scanOn_samp0.csv", row.names=FALSE)
+write.csv(scanOff_all_samp0, file = "all_scanOff_scanOn/all_scanOff_samp0.csv", row.names=FALSE)
+
+
+######################################
+## Writing a test function
+######################################
+
+printsum <- function(sum) {
+  print(paste0("The sum is: ",sum))
+}
+
+sum.of.squares <- function(x,y) {
+  sum <- x^2 + y^2
+  printsum(sum)
+}
+
+sum.of.squares(3,9)
+
+
+######################################################################
+## 01. CLEAN FILES 
+######################################################################
+
+all_scanOn_samp0 <- read_csv("all_scanOff_scanOn/all_scanOn_samp0.csv", col_names = TRUE)
+all_scanOff_samp0 <- read_csv("all_scanOff_scanOn/all_scanOff_samp0.csv", col_names = TRUE)
+head(all_scanOn_samp0)
+head(all_scanOff_samp0)
+
+# looking at calender 
+head(calendar)
+
+
+
+
+
+
+
+
+######################################
+## FOR EACH DATASET
+######################################
+# check all datatypes of each column 
+# mutate/split any columns that are incorrect (each value should be in its own column)
+# checking typos/ data entry errors or data import errors
+# check extra whitespace on values
+# check text upper/ lower case
+# check sanity checks
+# check and replace missing values
+# check and replace outliers 
+
+
+
+
+str(calendar)
+
---
+++ 01_import_clean_joining_tapontapoff.R
@@ -0,0 +1,172 @@
+######################################################################
+## PACKAGES
+######################################################################
+library(readr)
+library(data.table)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(beepr)
+library(magrittr)
+library(ggplot2)
+library(ggmap)
+library(RColorBrewer)
+library(rvest)
+
+
+## set your own working directory 
+setwd("/Users/ellenitoumpas/Dropbox/DATA/DATATHON/datathon/melbourne-datathon/")
+
+
+######################################################################
+## IMPORT FILES (except touch on and touch off)
+######################################################################
+calendar <- read.table("calendar.txt", sep="|", header=FALSE, col.names = c("DateSequence", "Date" ,"CalendarYear" ,"FinancialYear" ,"FinancialMonth" ,"CalendarMonth" ,"CalendarMonthSeq" ,"CalendarQuarter" ,"FinancialQuarter" ,"CalendarWeek" ,"FinancialWeek" ,"DayType" ,"DayTypeCategory", "DayTypeCategoryMore","WeekdaySeq" ,"WeekDay" ,"FinancialMonthSeq" ,"FinancialMonthName" ,"MonthNumber" ,"ABSWeek" ,"WeekEnding" ,"QuarterName"))
+car_speeds <- read_csv("car_speeds/melbourne_vehicle_traffic.csv")
+card_type <- read.table("card_types.txt", sep="|", header=FALSE, col.names = c("Card_SubType_ID" , "Card_SubType_Desc", "Payment_Type", "Fare_Type", "Concession_Type", "MI_Card_Group"))
+stop_locations <- read_delim("stop_locations.txt", "|", escape_double = FALSE, trim_ws = TRUE, col_names = FALSE)
+colnames(stop_locations) <- c("StopLocationID" , "StopNameShort", "StopNameLong", "StopType", "SuburbName", "PostCode", "RegionName", "LocalGovernmentArea", "StatDivision", "GPSLat", "GPSLong")
+
+
+######################################################################
+## IMPORT TXT FILES IN SUBFOLDERS - COPIED FROM ORGANISERS NOTES
+######################################################################
+
+#tell R where it can find the data
+ScanOnFolderMaster <- 'Samp_X/ScanOnTransaction'
+ScanOffFolderMaster <- 'Samp_X/ScanOffTransaction'
+
+# define if we want sample 0 or sample 1
+mySamp <- 1
+
+ScanOnFolder <- sub("X",mySamp,ScanOnFolderMaster)
+ScanOffFolder <- sub("X",mySamp,ScanOffFolderMaster)
+
+#list the files
+onFiles <- list.files(ScanOnFolder,recursive = TRUE,full.names = TRUE)
+offFiles <- list.files(ScanOffFolder,recursive = TRUE,full.names = TRUE)
+
+#how many touch off files we have
+cat("\nthere are", length(offFiles),'files')
+
+# set up an empty dataframe for all of touch off data
+scanOff_all_samp1 <- data.frame(Mode=integer(),
+                                BusinessDate=character(), 
+                                DateTime=character(), 
+                                CardID = integer(),
+                                CardType = integer(),
+                                VehicleID = integer(),
+                                ParentRoute = character(),
+                                RouteID = integer(),
+                                StopID = integer(),
+                   stringsAsFactors=FALSE) 
+
+scanOn_all_samp1 <- data.frame(Mode=integer(),
+                                BusinessDate=character(), 
+                                DateTime=character(), 
+                                CardID = integer(),
+                                CardType = integer(),
+                                VehicleID = integer(),
+                                ParentRoute = character(),
+                                RouteID = integer(),
+                                StopID = integer(),
+                                stringsAsFactors=FALSE)
+
+#---------------------------------------------------
+#read in all touch off files and collate to one file
+#---------------------------------------------------
+
+for (i in 1:157){
+  
+  # which file are we importing
+  myFile <- offFiles[i]
+ 
+  # import file
+  cmd <- paste0("gzip -dc ", myFile)
+  dt <- fread(cmd,nrow=10000,sep="|",fill=TRUE)
+ 
+  # bind imported file to the main dataframe
+  scanOff_all_samp1 <- rbind(as.data.frame(scanOff_all_samp1), as.data.frame(dt))
+   
+}
+
+colnames(scanOff_all_samp1) <- c('Mode','BusinessDate','DateTime','CardID','CardType','VehicleID','ParentRoute','RouteID','StopID')
+head(scanOff_all_samp1)
+beep()
+
+#---------------------------------------------------
+#read in all touch on files and collate to one file
+#---------------------------------------------------
+
+for (i in 1:157){
+  # which file are we importing
+  myFile <- onFiles[i]
+ 
+  # import file
+  cmd <- paste0("gzip -dc ", myFile)
+  dt <- fread(cmd,nrow=10000,sep="|",fill=TRUE)
+ 
+  # bind imported file to the main dataframe
+  scanOn_all_samp1 <- rbind(as.data.frame(scanOn_all_samp1), as.data.frame(dt))
+ 
+}
+colnames(scanOn_all_samp1) <- c('Mode','BusinessDate','DateTime','CardID','CardType','VehicleID','ParentRoute','RouteID','StopID')
+head(scanOn_all_samp1)
+
+
+beep()
+
+
+write.csv(scanOn_all_samp1, file = "all_scanOff_scanOn/all_scanOn_samp1.csv", row.names=FALSE)
+write.csv(scanOff_all_samp1, file = "all_scanOff_scanOn/all_scanOff_samp1.csv", row.names=FALSE)
+
+
+######################################################################
+## 01. CLEAN FILES 
+######################################################################
+all_scanOn_samp0 <- read_csv("all_scanOff_scanOn/all_scanOn_samp0.csv", col_names = TRUE)
+all_scanOff_samp0 <- read_csv("all_scanOff_scanOn/all_scanOff_samp0.csv", col_names = TRUE)
+all_scanOn_samp1 <- read_csv("all_scanOff_scanOn/all_scanOn_samp1.csv", col_names = TRUE)
+all_scanOff_samp1 <- read_csv("all_scanOff_scanOn/all_scanOff_samp1.csv", col_names = TRUE)
+
+head(all_scanOn_samp0)
+head(all_scanOff_samp0)
+head(all_scanOn_samp1)
+head(all_scanOff_samp1)
+
+
+
+
+######################################################################
+## LOOKING AT SAMP_0 + SAMP_1 DATA
+######################################################################
+
+
+# add ALL touch on and touch off data together
+# First step: add columns to keep track of where the data came from
+all_scanOn_samp0 <- all_scanOn_samp0 %>% mutate(source='touchon', sample="0")
+all_scanOn_samp1 <- all_scanOn_samp1 %>% mutate(source='touchon', sample="1")
+all_scanOff_samp0 <- all_scanOff_samp0 %>% mutate(source='touchoff', sample="0")
+all_scanOff_samp1 <- all_scanOff_samp1 %>% mutate(source='touchoff', sample="1")
+
+# Second step: combine rows into all_data
+all_data <- bind_rows(all_scanOn_samp0,all_scanOn_samp1)
+all_data <- bind_rows(all_data,all_scanOff_samp0)
+all_data <- bind_rows(all_data,all_scanOff_samp1)
+
+# Third step: checking the dimensions of all the datasets and then checking the all_data
+dim(all_scanOn_samp0)
+dim(all_scanOn_samp1)
+dim(all_scanOff_samp0)
+dim(all_scanOff_samp1)
+nrow(all_scanOn_samp0)*4
+dim(all_data)
+head(all_data)
+
+# Fourth step: write to external file just in case
+write.csv(all_data, file = "all_scanOff_scanOn/all_data_combined.csv", row.names=FALSE)
+
+head(all_data)
+head(stop_locations)
+
